62695d35bd8efc6baa93e09b7d08cb43f20772e9
Anton Lvov <aplvovnsu@gmail.com>
2021-09-16 14:47:56 +0700

    done

--- main.py
+++ main.py
@@ -1,16 +1,78 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# -*- coding: utf-8 -*-
+from collections import defaultdict
+from textblob import TextBlob
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def count_words(text) -> dict:
+    """
+    В самом примитивном варианте эта функция будет лепить к словам знаки
+    препинания, от которых они не отделены
+    В принципе, можно сначала удалить все знаки препинания функцией
+    remove_punctuation, а затем уже вызвать эту функцию от
+    нового текста
+    А ещё можно установить textblob
+    :param text: строка пользователя
+    :return: словарь частот слов
+    """
+    return TextBlob(text).word_counts
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def remove_words(text) -> str:
+    """
+    Удаляет слова "a", "the", "any"
+    Опять-таки, можем удалить знаки препинания, а то вхождения вроде
+    "any," останутся в тексте
+    или можно установить TextBlob
+    :param text: строка пользователя
+    :return: новая строка
+    """
+
+    banned_words = {'a', 'the', 'any'}
+
+    return ' '.join([word for word in TextBlob(text).words
+                     if word not in banned_words])
+
+
+def remove_punctuation(text) -> str:
+    """
+    Удаляет все знаки препинания из текста (на самом деле, непонятно,
+    что считать знаками препинания. Нужно ли удалять апостроф из "I'm",
+    пробелы, дефисы и т.д.
+    Я считал, что удаляем заранее зафиксированный набор символов
+    :param text: текст (строка) пользователя
+    :return: новый текст (строка) без знаков препинания
+    """
+    new_text = []
+    banned_symbols = {':', ';', '.', ',', '?', '!', '—',
+                      '\'', '\"', '(', ')', '[', ']', '{', '}'}
+    for char in text:
+        if char not in banned_symbols:
+            new_text.append(char)
+
+    return ''.join(new_text)
+
+
+def count_phrase(text, phrase) -> int:
+    """
+    можно просто вызвать text.count(phrase)
+    можно text.lower().count(phrase.lower()), чтобы учесть регистр
+    можно удалить все знаки препинания, а потом искать
+    если мы ищем именно предложение, можно сделать сплит по точкам
+    безграничные возможности
+    или textblob
+    :param text: строка, в которой ищем текст
+    :param phrase: текст, который ищем
+    :return: количество вхождений
+    """
+    return TextBlob(text).noun_phrases.count(phrase)
+
+
+def list_phrases_with_nouns(text) -> list:
+    """
+    видимо, имеется в виду поиск предложений с существительными
+    замечу, что это почти любое предложение
+    воспользуемся силой интернета и нестандартных библиотек
+    :param text: строка пользователя
+    :return: список предложений, в которых есть существительные
+    """
+    return TextBlob(text).noun_phrases
